c6b2f32a4d58e2efd713c63642b3c825be6d36b8
Sarah Balatbat <scsb1119@gmail.com>
2025-05-10 14:12:48 -0700

    Progress on Pt 3. Unit Testing with Jest: passing tests for unit-test-me.js:isDate

--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -51,5 +51,17 @@ test('Jx7cn9EEoZJn_OB3X68Y is not a strong password', () => {
 });
 
 /* isDate tests */
+test('5/10/2025 is a valid date', () => {
+  expect(isDate('5/10/2025')).toBe(true);
+});
+test('93/1/2077 is a valid date', () => {
+  expect(isDate('93/1/2077')).toBe(true);
+});
+test('5/10/25 is not a valid date', () => {
+  expect(isDate('5/10/25')).toBe(false);
+});
+test('January 5, 2016 is not a valid date', () => {
+  expect(isDate('January 5, 2016')).toBe(false);
+});
 
 /* isHexColor tests */

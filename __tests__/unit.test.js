37d690352682a0431714f61919653a41aa94bfd5
Sarah Balatbat <scsb1119@gmail.com>
2025-05-10 13:24:22 -0700

    Progress on Pt 3. Unit Testing with Jest: passing tests for unit-test-me.js:isStrongPassword

--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -8,7 +8,7 @@ import {
   isHexColor,
 } from '../code-to-unit-test/unit-test-me';
 
-// TODO - Part 2
+/* isPhoneNumber tests */
 test('(863) 123-4567 is a phone number', () => {
   expect(isPhoneNumber('(863) 123-4567')).toBe(true);
 });
@@ -21,3 +21,23 @@ test('abcdefg is not a phone number', () => {
 test('555 678 0925 is not a phone number', () => {
   expect(isPhoneNumber('555 678 0925')).toBe(false);
 });
+
+/* isEmail tests */
+
+/* isStrongPassword tests */
+test('wzOS_4FYsg is a strong password', () => {
+  expect(isStrongPassword('wzOS_4FYsg')).toBe(true);
+});
+test('Iyhskp83SveC1c is a strong password', () => {
+  expect(isStrongPassword('Iyhskp83SveC1c')).toBe(true);
+});
+test('0x1d_boomPOW is not a strong password', () => {
+  expect(isStrongPassword('0x1d_boomPOW')).toBe(false);
+});
+test('Jx7cn9EEoZJn_OB3X68Y is not a strong password', () => {
+  expect(isStrongPassword('Jx7cn9EEoZJn_OB3X68Y')).toBe(false);
+});
+
+/* isDate tests */
+
+/* isHexColor tests */
